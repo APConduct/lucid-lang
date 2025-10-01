2ba185b710b881b8a81dea0c6ec8d7737dd067bd
Perry <aidanpj18@gmail.com>
2025-10-01 06:48:59 -0400

    Add AST definitions for expressions, statements, and types

---
+++ src/ast.rs
@@ -0,0 +1,185 @@
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Type {
+    Number,
+    String,
+    Boolean,
+    Nil,
+    Function(Vec<Type>, Box<Type>),
+    Table,
+    Any,
+    UserDefined(String),
+}
+
+impl Type {
+    fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "number" => Some(Type::Number),
+            "string" => Some(Type::String),
+            "boolean" => Some(Type::Boolean),
+            "nil" => Some(Type::Nil),
+            "function" => Some(Type::Function(vec![], Box::new(Type::Any))),
+            "table" => Some(Type::Table),
+            "any" => Some(Type::Any),
+            _ => Some(Type::UserDefined(s.to_string())),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TypeIdent {
+    pub name: String,
+    pub ty: Option<Type>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Number(f64),
+    String(String),
+    Boolean(bool),
+    Nil,
+    Ident(String),
+    BinOp {
+        left: Box<Expr>,
+        op: BinOp,
+        right: Box<Expr>,
+    },
+    Call {
+        func: Box<Expr>,
+        args: Vec<Expr>,
+    },
+    Table(Vec<TableField>),
+    Index {
+        table: Box<Expr>,
+        index: Box<Expr>,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub struct TableField {
+    pub key: Option<Expr>,
+    pub value: Expr,
+}
+
+#[derive(Debug, Clone)]
+pub enum BinOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Eq,
+    Ne,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+    And,
+    Or,
+    Concat,
+}
+
+#[derive(Debug, Clone)]
+pub enum Stmt {
+    Local {
+        vars: Vec<TypeIdent>,
+        init: Option<Vec<Expr>>,
+    },
+    Assign {
+        targets: Vec<Expr>,
+        values: Vec<Expr>,
+    },
+    FunctionDecl {
+        name: String,
+        params: Vec<TypeIdent>,
+        return_type: Option<Type>,
+        body: Vec<Stmt>,
+    },
+    If {
+        condition: Expr,
+        then_block: Vec<Stmt>,
+        else_block: Option<Vec<Stmt>>,
+    },
+    While {
+        condition: Expr,
+        body: Vec<Stmt>,
+    },
+    Return(Vec<Expr>),
+    Expr(Expr),
+    // For {
+    //     var: TypeIdent,
+    //     start: Expr,
+    //     end: Expr,
+    //     step: Option<Expr>,
+    //     body: Vec<Stmt>,
+    // },
+}
+
+#[derive(Debug)]
+pub struct Program {
+    pub statements: Vec<Stmt>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_program() {
+        let program = Program {
+            statements: vec![Stmt::Expr(Expr::Number(42.0))],
+        };
+        assert_eq!(program.statements.len(), 1);
+    }
+
+    #[test]
+    fn test_program_with_statements() {
+        let program = Program {
+            statements: vec![
+                Stmt::Expr(Expr::Number(42.0)),
+                Stmt::Expr(Expr::String("Hello".to_string())),
+            ],
+        };
+        assert_eq!(program.statements.len(), 2);
+    }
+
+    #[test]
+    fn test_program_with_function() {
+        let program = Program {
+            statements: vec![Stmt::FunctionDecl {
+                name: "add".to_string(),
+                params: vec![
+                    TypeIdent {
+                        name: "x".to_string(),
+                        ty: Some(Type::Number),
+                    },
+                    TypeIdent {
+                        name: "y".to_string(),
+                        ty: Some(Type::Number),
+                    },
+                ],
+                return_type: Some(Type::Number),
+                body: vec![Stmt::Return(vec![Expr::BinOp {
+                    left: Box::new(Expr::Ident("x".to_string())),
+                    op: BinOp::Add,
+                    right: Box::new(Expr::Ident("y".to_string())),
+                }])],
+            }],
+        };
+        assert_eq!(program.statements.len(), 1);
+    }
+
+    #[test]
+    fn test_program_with_if_statement() {
+        let program = Program {
+            statements: vec![Stmt::If {
+                condition: Expr::Boolean(true),
+                then_block: vec![Stmt::Expr(Expr::Number(1.0))],
+                else_block: None,
+            }],
+        };
+        assert_eq!(program.statements.len(), 1);
+    }
+}
